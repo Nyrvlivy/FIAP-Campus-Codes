5c476e6ca32a4ba43401aaaf3b762d2a105b7e40
Nyrvlivy <nyrvlivy@gmail.com>
2024-02-02 16:38:54 -0300

    feat: Add algorithm to calculate basic operators in Python

---
+++ 00-foundations/ch-1-introduction/Python/operator_algorithm.py
@@ -0,0 +1,14 @@
+n1 = int(input('Enter first number: '))
+n2 = int(input('Enter second number: '))
+
+addition = n1 + n2
+subtraction = n1 - n2
+multiplication = n1 * n2
+division = n1 / n2
+
+print()
+
+print(f'Addition: {n1} + {n2} = {addition}')
+print(f'Subtraction: {n1} - {n2} = {subtraction}')
+print(f'Multiplication: {n1} * {n2} = {multiplication}')
+print(f'Division: {n1} / {n2} = {division}')
