5fbdd431597453a64d3ce8b6f518011ad4214af2
Nyrvlivy <nyrvlivy@gmail.com>
2024-02-17 20:32:34 -0300

    refactor: Improve income tax calculation using tax brackets in Java

--- Java/src/br/com/fiap/m00_foundations/ch03_conditional_statements/IncomeTaxCalculator.java
+++ Java/src/br/com/fiap/m00_foundations/ch03_conditional_statements/IncomeTaxCalculator.java
@@ -1,30 +1,59 @@
 package br.com.fiap.m00_foundations.ch03_conditional_statements;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class IncomeTaxCalculator {
     private static final Scanner scanner = new Scanner(System.in);
+    private static final List<TaxBracket> taxBrackets = new ArrayList<>();
+
+    static {
+        taxBrackets.add(new TaxBracket(new BigDecimal("2259.20"), BigDecimal.ZERO));
+        taxBrackets.add(new TaxBracket(new BigDecimal("2826.65"), (new BigDecimal("0.075"))));
+        taxBrackets.add(new TaxBracket(new BigDecimal("3751.05"), (new BigDecimal("0.15"))));
+        taxBrackets.add(new TaxBracket(new BigDecimal("4664.68"), (new BigDecimal("0.225"))));
+        taxBrackets.add(new TaxBracket(null, (new BigDecimal("0.275"))));
+    }
+
+    private static class TaxBracket {
+        BigDecimal limit;
+        BigDecimal rate;
+
+        public TaxBracket(BigDecimal limit, BigDecimal rate) {
+            this.limit = limit;
+            this.rate = rate;
+        }
+    }
 
     private static BigDecimal readSalary() {
-        BigDecimal salary = BigDecimal.ZERO;
-        boolean validInput = false;
-
-        while (!validInput) {
-            System.out.print("Enter your salary: ");
+        BigDecimal salary;
+        while (true) {
+            System.out.print("\nEnter your salary: ");
             if (scanner.hasNextBigDecimal()) {
                 salary = scanner.nextBigDecimal();
-                if (salary.compareTo(BigDecimal.ZERO) > 0) {
-                    validInput = true;
-                } else {
-                    System.out.println("Invalid input. Please enter a valid salary.");
-                }
+                if (salary.compareTo(BigDecimal.ZERO) > 0) break;
+                else System.out.println("Invalid input. Please enter a valid salary.");
             } else {
                 System.out.println("Invalid input. Please enter a numeric value.");
                 scanner.next();
             }
         }
         return salary;
+    }
+
+
+    public void calculateIncomeTax(BigDecimal salary) {
+        BigDecimal ir = BigDecimal.ZERO;
+        for (TaxBracket bracket : taxBrackets) {
+            if (bracket.limit == null || salary.compareTo(bracket.limit) <= 0) {
+                ir = salary.multiply(bracket.rate);
+                break;
+            }
+        }
+
+        printDetails(salary, ir);
     }
 
     private void printDetails(BigDecimal salary, BigDecimal incomeTax) {
@@ -35,49 +64,27 @@ public class IncomeTaxCalculator {
         System.out.println("-----------------------------");
     }
 
-    public void calculateIncomeTax(BigDecimal salary) {
-        BigDecimal ir;
-        if (salary.compareTo(new BigDecimal("2259.20")) <= 0) {
-            ir = BigDecimal.ZERO;
-        } else if (salary.compareTo(new BigDecimal("2826.65")) <= 0) {
-            ir = salary.multiply(new BigDecimal("0.075"));
-        } else if (salary.compareTo(new BigDecimal("3751.05")) <= 0) {
-            ir = salary.multiply(new BigDecimal("0.15"));
-        } else if (salary.compareTo(new BigDecimal("4664.68")) <= 0) {
-            ir = salary.multiply(new BigDecimal("0.225"));
-        } else {
-            ir = salary.multiply(new BigDecimal("0.275"));
-        }
-        printDetails(salary, ir);
-    }
-
     public static boolean exitProgram() {
-        String answer;
-        do {
+        while (true) {
             System.out.print("Do you want to calculate another salary? (Y/N) ");
-            answer = scanner.next();
-            if (!answer.equalsIgnoreCase("Y") && !answer.equalsIgnoreCase("N")) {
-                System.out.println("Invalid input. Please enter 'Y' or 'N'.");
-                System.out.println();
-            }
-        } while (!answer.equalsIgnoreCase("Y") && !answer.equalsIgnoreCase("N"));
-        return !answer.equalsIgnoreCase("Y");
+            String answer = scanner.next();
+            if (answer.equalsIgnoreCase("Y")) return false;
+            else if (answer.equalsIgnoreCase("N")) return true;
+            else System.out.println("Invalid input. Please enter 'Y' or 'N'.\n");
+        }
     }
 
     public static void main(String[] args) {
         System.out.println("==============================");
         System.out.println("Welcome to the Income Tax Calculator");
         System.out.println("==============================");
-        System.out.println();
 
-        boolean isExit = false;
         IncomeTaxCalculator incomeTaxCalculator = new IncomeTaxCalculator();
 
-        while (!isExit) {
+        do {
             BigDecimal salary = readSalary();
             incomeTaxCalculator.calculateIncomeTax(salary);
-            isExit = exitProgram();
-        }
+        } while (!exitProgram());
 
     }
 }
